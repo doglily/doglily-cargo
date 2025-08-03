9c4a16abab49e72b5aad215d9b94e91bbce30b4a
doglily <subin.web.github@gmail.com>
2025-08-03 21:40:11 +0900

    Update mobile.js

--- mobile.js
+++ mobile.js
@@ -92,6 +92,7 @@ function waitForIframeLoad(iframe) {
 // iframe에 버튼 붙이기
 async function setIframeAttributesAndAddButton(iframe) {
 	const src = iframe.getAttribute("src");
+	console.log(`src:`, src);
 	if (!src?.startsWith("https://player.vimeo.com")) return;
 
 	const url = new URL(src);
